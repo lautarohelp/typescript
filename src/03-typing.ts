0dd1e158d0740adb71f1717bc25105a281223d2a
lautarohelp <lautarotoledo391@gmail.com>
2024-01-19 15:53:28 -0600

    Tipos inferidos

---
+++ src/03-typing.ts
@@ -0,0 +1,16 @@
+(() => {
+  let myProductName = 'Product 1';
+  let myProductPrice = 123;
+  // si le asignamos que es un string estrictamente va a declarase como string a la hora de cambiarlo por un number te sugiere que lo mejor es segir trabajando con string.
+
+  myProductName = 'change';
+  myProductName.toLowerCase();
+
+  myProductPrice.toFixed();
+
+  const myProductStock = 1000;
+  const myProductName2 = 'Product 1';
+})();
+
+
+
